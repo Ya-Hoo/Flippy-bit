a90d6a31602f141d90e751cecaec81644134c825
Gia Phu Huynh <huynhgiaphu3004@gmail.com>
2023-11-16 22:34:31 +0700

    start to write test program

--- testing.py
+++ testing.py
@@ -1,1 +1,7 @@
-# write test program here
+import os
+
+directory = os.getcwd()
+bot_path = fr"{directory}\bot"
+data_path = fr"{directory}\data"
+
+test_version = input("Type in version to test: ").capitalize() + "-edByte"
